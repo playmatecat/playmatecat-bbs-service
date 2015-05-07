7f9477fec43b0719eab2f0690eaa5fa0f1d09bf2
blackcatIan <blackcatIan@hotmail.com>
2015-05-07 13:27:03 +0800

    1

--- playmatecat-bbs-service-front/src/main/java/com/playmatecat/cpt/UserCpt.java
+++ playmatecat-bbs-service-front/src/main/java/com/playmatecat/cpt/UserCpt.java
@@ -3,11 +3,10 @@ package com.playmatecat.cpt;
 import net.minidev.json.JSONObject;
 import net.minidev.json.JSONValue;
 
-import org.apache.commons.lang3.RandomUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.playmatecat.database.bbs.Article;
 import com.playmatecat.service.User;
 import com.playmatecat.service.UserService;
 
@@ -17,26 +16,26 @@ public class UserCpt {
 	@Autowired
 	private UserService userService;
 	
-	public String savetestCall() throws Exception{
-		Long start = System.currentTimeMillis();
-		User user = new User();
+	public String savetestCall(Article art) throws Exception{
+//		Long start = System.currentTimeMillis();
+//		User user = new User();
+//		
+//		userService.selectUserById(100000000);
+//		Long end = System.currentTimeMillis();
+//		//System.out.println("sql finshed in " + (end - start) + " ms");
+//
+//		user.setUsername(String.valueOf(RandomUtils.nextLong(0, 100000)));
+//		
+//		User tmpUser = new User();
+//		user.setUsername("testTx");
+////		userService.insertUser(tmpUser);
+//		
+////		if(StringUtils.isEmpty("")) {
+////			throw new Exception("test tx error");
+////		}
+		System.out.println(art.getContent());
 		
-		userService.selectUserById(100000000);
-		Long end = System.currentTimeMillis();
-		//System.out.println("sql finshed in " + (end - start) + " ms");
-
-		user.setUsername(String.valueOf(RandomUtils.nextLong(0, 100000)));
-		
-		User tmpUser = new User();
-		user.setUsername("testTx");
-//		userService.insertUser(tmpUser);
-		
-//		if(StringUtils.isEmpty("")) {
-//			throw new Exception("test tx error");
-//		}
-		
-		
-		return JSONValue.toJSONString(user);
+		return JSONValue.toJSONString(art);
 	}
 	
 	
